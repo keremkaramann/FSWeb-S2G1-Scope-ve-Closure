f92b8209af0da928fe81d65271edcdae443a8a0a
Kerem Karaman <keremkaraman91@gmail.com>
2023-07-31 15:46:00 +0300

    testler yapıldı

--- index.js
+++ index.js
@@ -3,25 +3,29 @@
 // Başlangıç Challenge'ı
 
 /**Örnek Görev: İlkini Dön
- * 
+ *
  * Bu örnek sonradan gelecek olan görevleri nasıl çözeceğinizi size gösterecek.
- * 
+ *
  * Aşağdıaki Yüksek dereceden fonskiyonu(higher-order function) kullanarak aşağıdakileri yapınız
  *  1. Stringlerden oluşan bir array'i parametre olarak alın
- *  2. Bir string'i değişken olarak alan bir callback fonksiyonunu parametre olarak alın 
+ *  2. Bir string'i değişken olarak alan bir callback fonksiyonunu parametre olarak alın
  *  3. Array'in İLK elemanını değişken olarak alarak çalışacak olan callback fonksiyonunun sonucunu dönün
- * 
+ *
  * Aşağıdaki kodlar bu görevin nasıl yapılacağına örnek olacaktır
  * Bu fonskiyon 'asas' dönmeli(return)
-*/
+ */
 
 function ilkiniDon(stringArray, callback) {
-  return callback(stringArray[0])
+  return callback(stringArray[0]);
 }
-console.log('örnek görev:', ilkiniDon(['as','sa'],function(metin){return metin+metin}));
+console.log(
+  "örnek görev:",
+  ilkiniDon(["as", "sa"], function (metin) {
+    return metin + metin;
+  })
+);
 
 // Başlangıç Challenge'ı Sonu
-
 
 ///// M V P ///////
 
@@ -40,19 +44,19 @@ console.log('örnek görev:', ilkiniDon(['as','sa'],function(metin){return metin
 function skorArtirici() {
   let skor = 0;
   return function skorGuncelle() {
-   return skor++;
-  }
+    return skor++;
+  };
 }
 
 const skor1 = skorArtirici();
-
+// skor1 skorArtirici fonksiyonun aynısıdır, fonksiyon içinde yeni bir fonksiyon yani closer vardır.
 // skor2 kodları
 let skor = 0;
 
 function skor2() {
   return skor++;
 }
-
+//
 
 /* Görev 2: takimSkoru() 
 Aşağıdaki takimSkoru() fonksiyonununda aşağıdakileri yapınız:
@@ -64,12 +68,11 @@ Aşağıdaki takimSkoru() fonksiyonununda aşağıdakileri yapınız:
 Not: Bu fonskiyon, aşağıdaki diğer görevler için de bir callback fonksiyonu olarak da kullanılacak
 */
 
-function takimSkoru(/*Kodunuzu buraya yazınız*/){
-    /*Kodunuzu buraya yazınız*/
+function takimSkoru() {
+  /*Kodunuzu buraya yazınız*/
+  return Math.floor(Math.random() * (25 - 10) + 10);
 }
-
-
-
+//console.log(takimSkoru());
 
 /* Görev 3: macSonucu() 
 Aşağıdaki macSonucu() fonksiyonununda aşağıdakileri yapınız:
@@ -84,16 +87,21 @@ Aşağıdaki macSonucu() fonksiyonununda aşağıdakileri yapınız:
   "EvSahibi": 92,
   "KonukTakim": 80
 }
-*/ 
+*/
 
-function macSonucu(/*Kodunuzu buraya yazınız*/){
+function macSonucu(takimSkoru, num) {
   /*Kodunuzu buraya yazınız*/
+  let skor = {
+    EvSahibi: 0,
+    KonukTakim: 0,
+  };
+  for (let i = 1; i <= num; i++) {
+    skor.EvSahibi = takimSkoru();
+    skor.KonukTakim = takimSkoru();
+  }
+  return skor;
 }
-
-
-
-
-
+//console.log(macSonucu(takimSkoru, 2));
 
 /* Zorlayıcı Görev 4: periyotSkoru()
 Aşağıdaki periyotSkoru() fonksiyonununda aşağıdakileri yapınız:
@@ -108,20 +116,24 @@ Aşağıdaki periyotSkoru() fonksiyonununda aşağıdakileri yapınız:
 }
   */
 
-
-function periyotSkoru(/*Kodunuzu buraya yazınız*/) {
+function periyotSkoru(takimSkoru) {
   /*Kodunuzu buraya yazınız*/
-
+  let skor = {
+    EvSahibi: takimSkoru(),
+    KonukTakim: takimSkoru(),
+  };
+  return skor;
 }
-
-
+console.log(periyotSkoru(takimSkoru));
 /* Zorlayıcı Görev 5: skorTabelasi() 
 Aşağıdaki skorTabelasi() fonksiyonunu kullanarak aşağıdakileri yapınız:
   1. İlk parametre olarak Görev 4'te oluşturduğumuz 'periyotSkoru'nu bir değişken olarak almalı
   2. İkinci parametre olarak Gröev 2'de oluşturduğumuz 'takimSkoru'nu bir değişken olarak almalı
   3. Üçüncü parametre olarak da oynanacak olan çeyrek sayısını alın
-  4. Her bir çeyreğin sonucunu bir string olarak bir array içinde dönün. Aşadaki örnek gibi olmalı. Her çeyrekteki atılan sayıları ayrı ayrı yazmalı(toplam skoru değil!).
-  5. Eğer maç berabere biterse uzatmalar oynanmalı ve "Uzatma 1: Ev Sahibi 13 - Konuk Takım 11" eklemeli. (Her uzatma için ayrı ayrı eklemeli)
+  4. Her bir çeyreğin sonucunu bir string olarak bir array içinde dönün. Aşadaki örnek gibi olmalı. 
+  Her çeyrekteki atılan sayıları ayrı ayrı yazmalı(toplam skoru değil!).
+  5. Eğer maç berabere biterse uzatmalar oynanmalı ve "Uzatma 1: Ev Sahibi 13 - Konuk Takım 11" eklemeli. 
+  (Her uzatma için ayrı ayrı eklemeli)
   6. Maç bitince de final skoru yazmalı: "Maç Sonucu: Ev Sahibi 101 - Konuk Takım 98"
 
 MAÇ UZAMAZ ise skorTabelasi(periyotSkoru,takimSkoru,4)
@@ -146,17 +158,42 @@ MAÇ UZAR ise skorTabelasi(periyotSkoru,takimSkoru,4)
 ] */
 // NOTE: Bununla ilgili bir test yoktur. Eğer logladığınız sonuçlar yukarıdakine benziyor ise tmamlandı sayabilirsiniz.
 
-function skorTabelasi(/*Kodunuzu buraya yazınız*/) {
+function skorTabelasi(periyotSkoru, takimSkoru, number) {
   /*Kodunuzu buraya yazınız*/
+  let periyotSkoru2 = periyotSkoru(takimSkoru);
+
+  let tumSkor = [];
+  let evSahibiToplam = 0;
+  let konukToplam = 0;
+  for (let i = 1; i <= number; i++) {
+    if (i >= 5) {
+      return console.log("Hatalı sayı!!!");
+    } else {
+      tumSkor.push(
+        `${i}. Periyot: Ev Sahibi ${periyotSkoru2.EvSahibi} - Konuk Takım ${periyotSkoru2.KonukTakim}`
+      );
+      evSahibiToplam += periyotSkoru2.EvSahibi;
+      konukToplam += periyotSkoru2.KonukTakim;
+    }
+  }
+
+  tumSkor.push(
+    `Maç Sonucu: Ev Sahibi ${evSahibiToplam} - Konuk Takım ${konukToplam}`
+  );
+  return tumSkor;
 }
+console.log("son console", skorTabelasi(periyotSkoru, takimSkoru, 4));
 
-
-
+/* `${i}. Periyot: Ev Sahibi ${
+          periyotSkoru2.EvSahibi + periyotSkoru2.EvSahibi
+        } - Konuk Takım ${
+          periyotSkoru2.KonukTakim + periyotSkoru2.KonukTakim
+        }`  (i === 4 && periyotSkoru2.EvSahibi === periyotSkoru2.KonukTakim)*/
 
 /* Aşağıdaki satırları lütfen değiştirmeyiniz*/
-function sa(){
-  console.log('Kodlar çalışıyor');
-  return 'as';
+function sa() {
+  console.log("Kodlar çalışıyor");
+  return "as";
 }
 sa();
 module.exports = {
@@ -168,4 +205,4 @@ module.exports = {
   macSonucu,
   periyotSkoru,
   skorTabelasi,
-}
+};
